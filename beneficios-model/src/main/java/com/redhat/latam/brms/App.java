8a6ef27d490f28688de139c28bbca280b4b4a35e
Adriel Paredes <adriel.paredes@gmail.com>
2012-12-17 17:24:31 -0300

    Se agrega el model y el motor de reglas de negocio

---
+++ beneficios-model/src/main/java/com/redhat/latam/brms/App.java
@@ -0,0 +1,13 @@
+package com.redhat.latam.brms;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
